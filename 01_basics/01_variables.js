fa07fd3fbd33c065f650a297093a67cedfbb0141
modirocky22031992-spec <modirocky22031992@gmail.com>
2026-02-02 10:36:58 +0000

    Rename old files and add datatypes files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountName = "Rocky"
+var accountEmail = "Rocky@test.com"
+accountPassword = "3453453456"
+accountCity = "Surat"
+
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let age = 18
+let name = "Rocky"
+let IsLoggedIn = true
+let abc
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stanalone value
+// undefined => 
+// symbol => unique (use in react js)
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined 
